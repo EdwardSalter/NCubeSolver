6b42cbcd2663adedd391304b6f2de4fc2392cd68
EdwardSalter <mayhem366@hotmail.com>
2014-05-28 20:33:59 +0100

    Changed assembly names in test folder.

--- Solvers.Tests/Properties/Settings.Designer.cs
+++ Solvers.Tests/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Solvers.UnitTests.Properties {
+namespace NCubeSolver.Plugins.Solvers.UnitTests.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
--- Solvers/Properties/AssemblyInfo.cs
+++ Solvers/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("Solvers.UnitTests")]
-[assembly: InternalsVisibleTo("Solvers.IntegrationTests")]
+[assembly: InternalsVisibleTo("Plugin.Solvers.UnitTests")]
+[assembly: InternalsVisibleTo("Plugin.Solvers.IntegrationTests")]
